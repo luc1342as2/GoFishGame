c7ac1b72b627c0f0d73e8241f61af718a6a98236
luc1342as2 <98180767+luc1342as2@users.noreply.github.com>
2022-08-05 10:03:02 -0400

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,10 @@
+/**  
+ * Lucas Ghigli 
+ * Card.java -> GoFish game
+ * Ronak Sheth
+ * 05/08/2022
+*/ 
+ enum Card {
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+}
+
